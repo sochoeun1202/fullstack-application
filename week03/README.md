de64f6ee94fe7bbbcef52e9c72a76fa245f51dbd
sochoeun khorn <150789500+sochoeun1202@users.noreply.github.com>
2026-05-10 13:24:55 +0700

    init project fullstack

---
+++ week03/README.md
@@ -0,0 +1,13 @@
+# Spring Boot Microservices with Spring Cloud
+
+## Docker Containers
+
+1. Running Application
+
+```bash
+# check compose configuration
+docker compose config
+
+# run application in detached mode
+docker compose up -d
+```
---
+++ week03/src/test/java/com/sochoeun/microservice/TracingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sochoeun.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TracingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
